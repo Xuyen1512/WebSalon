370fd43e11aedf515f8d85d6deb6d9e270a8089f
Xuyen1512 <muoixuyen7@gmail.com>
2025-07-09 10:40:43 +0700

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,38 @@
+<style> 
+    #footer {
+            background-color: #D2B48C;
+            display: flex;
+            justify-content: space-between;
+            padding: 30px;
+            height: auto;
+            flex-wrap: wrap;
+            clear:both;
+        }
+
+    .lienhe, .diachi {
+            width: 45%;
+        }
+</style>
+
+ <div id="footer">
+            <div class="lienhe">
+                <p><strong>Liên hệ:</strong></p>
+                <p>Hotline: 0909 888 999</p>
+                <p>Gmail: salonlanmay@gmail.com</p>
+                <p>Zalo: 0909 888 999</p>
+                <p><strong>Socials:</strong></p>
+                <p>Facebook: fb.com/salonlanmay</p>
+                <p>Tiktok: @salonlanmay</p>
+            </div>
+
+            <div class="diachi">
+                <p><strong> Địa chỉ các chi nhánh</strong></p>
+                <ul style="line-height: 2" >
+                    <li> CN1: 123 Lý Tự Trọng, Q1, TPHCM </li>
+                    <li> CN2: 456 Phan Xích Long, Q.Phú Nhuận, TPHCM</li>
+                    <li> CN3: 89 Trần Não, TP.Thủ Đức</li>
+                    <li> CN4: 202 Hai Bà Trưng, Phường 6, Q3, TPHCM </li>
+                    <li> CN5: 101 Nguyễn Văn Cừ, Phường; An Hòa, Quận Ninh Kiều, Cần Thơ</li>
+                </ul>
+            </div>
+        </div>
